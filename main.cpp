bcbed076528c7e0dd979c7229c024717074d4ed7
Pawel <pawelzeja@student.agh.edu.pl>
2023-05-30 13:56:18 +0200

    losowanie pozycji

--- main.cpp
+++ main.cpp
@@ -4,3 +4,24 @@ int main() {
     mina.debug_display();
     return 0;
 }
+
+void losuj_pozycje ()
+{
+    time_t t;
+    int poz_x, poz_y;
+    int ilosc = 10;
+
+    srand((unsigned)time(&t));
+
+    while (ilosc>0)
+    {
+        poz_x = rand()%10;
+        poz_y = rand()%10;
+
+        if (plansza[poz_x][poz_y].wartosc!=9)
+        {
+            ustaw_mine(poz_x,poz_y);
+            ilosc--;
+        }
+    }
+}

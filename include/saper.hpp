7d886eafc8212b36de65f191c4f1db06bc678dfe
Pawel <pawelzeja@student.agh.edu.pl>
2023-05-30 13:37:24 +0200

    Clion konfiguracja

---
+++ include/saper.hpp
@@ -0,0 +1,16 @@
+//
+// Created by pawel on 30.05.2023.
+//
+
+#ifndef PROJEKT_GRY_SAPER_SAPER_HPP
+#define PROJEKT_GRY_SAPER_SAPER_HPP
+
+
+
+
+
+
+
+
+
+#endif //PROJEKT_GRY_SAPER_SAPER_HPP
---
+++ src/saper.cpp
@@ -0,0 +1,9 @@
+//
+// Created by pawel on 30.05.2023.
+//
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}

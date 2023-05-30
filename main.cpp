6f6f02dc02fa5e467c50a23a47c567d614da7b67
pawelzeja098 <130493752+pawelzeja098@users.noreply.github.com>
2023-05-30 14:01:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,3 +4,24 @@ int main() {
     mina.debug_display();
     return 0;
 }
+
+void position ()
+{
+    time_t t;
+    int poz_x, poz_y;
+    int ilosc = 10;
+
+    srand((unsigned)time(&t));
+
+    while (ilosc>0)
+    {
+        poz_x = rand()%10;
+        poz_y = rand()%10;
+
+        if (plansza[poz_x][poz_y].wartosc!=9)
+        {
+            set_mine(poz_x,poz_y);
+            ilosc--;
+        }
+    }
+}

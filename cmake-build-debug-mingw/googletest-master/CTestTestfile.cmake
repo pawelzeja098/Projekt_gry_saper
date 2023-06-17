65acd1d7fedb8f6f59609a0575966c441a66079b
SirPapkin <piotr8zajac8@gmail.com>
2023-06-17 21:58:16 +0200

    Działanie wspólne

---
+++ cmake-build-debug-mingw/googletest-master/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Piotrek/Desktop/Materiały/obiektowka/cpp/mdig-agh-lang_cpp__exercises-288bd81c85d7/solutions/Projekt_gry_saper/googletest-master
+# Build directory: C:/Users/Piotrek/Desktop/Materiały/obiektowka/cpp/mdig-agh-lang_cpp__exercises-288bd81c85d7/solutions/Projekt_gry_saper/cmake-build-debug-mingw/googletest-master
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ cmake-build-debug-mingw/googletest-master/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Piotrek/Desktop/Materiały/obiektowka/cpp/mdig-agh-lang_cpp__exercises-288bd81c85d7/solutions/Projekt_gry_saper/googletest-master/googletest
+# Build directory: C:/Users/Piotrek/Desktop/Materiały/obiektowka/cpp/mdig-agh-lang_cpp__exercises-288bd81c85d7/solutions/Projekt_gry_saper/cmake-build-debug-mingw/googletest-master/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

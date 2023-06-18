cabbdbb19b2b46120ee6dbc3b869904f6e4530bd
Wyrwinho <129854995+Wyrwinho@users.noreply.github.com>
2023-06-18 14:40:25 +0200

    miny_sąsiadujące

--- include/saper.hpp
+++ include/saper.hpp
@@ -22,53 +22,12 @@ void makeMove(int *x, int *y);
 
 void printBoard(char myBoard[][MAXSIDE]);
 
+int countAdjacentMines(int row, int col, int mines[][2], char realBoard[][MAXSIDE]);
 
-//        if (mode == EASY){difficulty = static_cast<float>(std::ceil(size_ * 0.1));}
-//        else if (mode == NORMAL){difficulty = static_cast<float>(std::ceil(size_ * 0.2));}
-//        else if (mode == HARD){difficulty = static_cast<float>(std::ceil(size_ * 0.3));}
-//        std::cout<< "Mine ma "<<difficulty<<" pol" << std::endl;
-//
-//// tworzę wektor, który ma tyle miejsc ile jest pól, a następnie mieszam go i biorę difficulty pierwszych miejsc
-//        std::vector<int> numer_pola ;
-//        for (int i = 0; i < size_; i++) {
-//            numer_pola.push_back(1+i)     ;
-//        }
-////wymieszańsko
+////wymieszanie
 //        std::random_device rd;
 //        std::mt19937 g(rd());
 //        std::shuffle(numer_pola.begin(), numer_pola.end(), g);
-//
-//// Ustawienie min na wylosowanych polach
-//        for (std::size_t i = 0; i < (std::size_t) difficulty; i++) {
-//            int row = (numer_pola[i] - 1) / width_;
-//            int col = (numer_pola[i] - 1) % width_;
-//            board_[row][col].hasMine_ = true;
-//        }
-//
-//
-//        //wypisanie wektora pól
-//        for (const auto& element : numer_pola) {
-//            std::cout << element << " ";
-//        }
-//        std::cout<< std::endl;
-//
-//    }
-//
-//    int get_width() const {
-//        return width_;
-//    }
-//    int get_height() const {
-//        return height_;
-//    }
-//
-//
-//    void debug_display() const; // uwaga przy deklarowaniu z const, tak sie robi żeby w src definiować
-//
-//    bool is_game_over() const;
-//    /*
-//     * funkcja losuje pozycje na planszy zwraca numer pola
-//     */
-//    int random_position();
 //
 ///*
 // * funckja zwiekszajaca wynik
